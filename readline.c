c2ebd2a5129c573e5f5b14afb029108a5b08cbe5
MartinJohaa <martin.johansson17@gmail.com>
2014-10-17 15:39:11 +0200

    fixed readline.c merge

--- readline.c
+++ readline.c
@@ -1,7 +1,6 @@
 #include "readline.h"
 #include <string.h>
 
-<<<<<<< HEAD
 int count = 0;
 
 void readline(char *dest, int n, FILE *source){
@@ -10,11 +9,4 @@ void readline(char *dest, int n, FILE *source){
   if(dest[len-1] == '\n')
     dest[len-1] = '\0';
   }else count++;
-=======
-void readline(char *dest, int n, FILE *source){
-  fgets(dest, n, source);
-  int len = strlen(dest);
-  if(dest[len-1] == '\n')
-    dest[len-1] = '\0';
->>>>>>> 1ee905749c4553e5acf3de72c734dede009b3c36
 }
